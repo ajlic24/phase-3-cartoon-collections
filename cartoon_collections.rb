ae29e33ae9a2304de906a18b2170b0d7ba73d07b
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-16 20:19:48 -0500

    commit tree 7b766e77d4eab328549b5a392b6ce417f133a878

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  arr.each.with_index do |dwarf, i|
+    puts "#{i + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map do |ele|
+    ele.capitalize + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  compare = []
+  arr.each do |ele|
+    if ele.length > 4
+      compare << ele
+    end
+  end
+  compare.length > 0
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ans = arr.find do |food|
+    cheese_types.include?(food)
+  end
+  ans ? ans : nil
 end
